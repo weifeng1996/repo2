fc1645859e710b4333f7795c2a2c183e4a6e1a44
weifeng <843339887@qq.com>
2019-11-30 21:10:13 +0800

    用wf分支进行提交的

--- HelloProjet/src/java/suibian.java
+++ HelloProjet/src/java/suibian.java
@@ -1,5 +1,15 @@
 public class suibian {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(
+                "小可爱" +
+                "我好想你" +
+                "都不跟我打视频" +
+                "不爱了" +
+                "就是这样的" +
+                "哼！！！");
+
+        System.out.println("这是用wf分支进行提交的" +
+                "？");
     }
 }
